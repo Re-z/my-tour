c522415787f588aa47f067df0fe3116fc2662de4
Re-z <re-z@ukr.net>
2017-02-01 19:16:15 +0200

    done

--- src/js/common.js
+++ src/js/common.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
-    $('.js-slider').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true
-    });
-    
+
     //mobile burger
     $('.header__burger').click(function () {
-        $('.header__nav').toggleClass('is-visible');
-    })
+        $('.nav').toggleClass('is-visible');
+    });
 
     //init datepicker
     $( "#datepicker" ).datepicker();
